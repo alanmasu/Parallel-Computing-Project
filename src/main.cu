3456262a06ebc377c72915dc02382db62c3b947b
alanmasu <alan.masutti@gmail.com>
2024-09-16 19:10:50 +0200

    Added other prints and added to GIT also 2 run.out whit my notes [README]
     - I've added simple notes to .out files so I can remember how to do batched MatMul

--- src/main.cu
+++ src/main.cu
@@ -165,17 +165,24 @@ int main(int argc, char **argv) {
 #warning "Testing mode"
 
 __global__ void simpleKernel(int n, int bs){
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
-    printf("a[%d][%d] X b[%d][%d]\n", row, col, col, row);
-    
+    const int N_BLOCKS = n / bs;
+    printf("blockDim.y: %d\n", blockDim.y);
+    printf("blockDim.x: %d\n", blockDim.x);
+    printf("gridDim.y: %d\n", gridDim.y);
+    printf("gridDim.x: %d\n", gridDim.x);
+
+    int bRow = blockIdx.y * blockDim.y + threadIdx.y;
+    int bCol = blockIdx.x * blockDim.x + threadIdx.x;
+    for(int i = 0; i < N_BLOCKS; ++i){
+        printf("c'[%d][%d] = a[%d][%d] X b[%d][%d]\n", bRow, bCol, bRow, i, i, bCol);
+    }
 }
 
 int main(int argc, char **argv){
-    // dim3 threadsPerBlock(32, 32);
+    dim3 threadsPerBlock(1, 1);
     dim3 numBlocks(N / BS, N / BS);
 
-    simpleKernel<<<numBlocks,1>>>(N, BS); 
+    simpleKernel<<<numBlocks,threadsPerBlock>>>(N, BS); 
     cudaDeviceSynchronize();
     return 0;
 }
