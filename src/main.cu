72c290066ed1d400a87ef1f44211d5f50c69f5b7
alanmasu <alan.masutti@gmail.com>
2024-09-16 11:21:17 +0200

    Uncommented code

--- src/main.cu
+++ src/main.cu
@@ -154,117 +154,119 @@ int main(int argc, char **argv) {
 
 int main(int argc, char **argv){
     printf("WMMA TEST: Testing mode\n");
-    // // Puntatori per le matrici sull'host
-    // float *h_A = NULL;
-    // float *h_B = NULL;
-    // float *h_C_cublas = NULL;
-    // float *h_C_wmma = NULL;
+    // Puntatori per le matrici sull'host
+    float *h_A = NULL;
+    float *h_B = NULL;
+    float *h_C_cublas = NULL;
+    float *h_C_wmma = NULL;
 
     // // Allocazione delle matrici sull'host (CPU)
-    // printf("[INFO]: Allocazione delle matrici sull'host\n");
-    // size_t matrix_size = N * N * sizeof(float);
-    // h_A = (float *)malloc(matrix_size);
-    // h_B = (float *)malloc(matrix_size);
-    // h_C_cublas = (float *)malloc(matrix_size);
-    // h_C_wmma = (float *)malloc(matrix_size);
-    // printf("[INFO]: Allocazione delle matrici sull'host completata\n");
+    printf("[INFO]: Allocazione delle matrici sull'host\n");
+    size_t matrix_size = N * N * sizeof(float);
+    h_A = (float *)malloc(matrix_size);
+    h_B = (float *)malloc(matrix_size);
+    h_C_cublas = (float *)malloc(matrix_size);
+    h_C_wmma = (float *)malloc(matrix_size);
+    printf("[INFO]: Allocazione delle matrici sull'host completata\n");
 
-    // // Inizializza le matrici A e B sull'host
-    // if(h_A != NULL && h_B != NULL){
-    //     printf("[INFO]: Inizializzazione delle matrici sull'host\n");
-    //     for (int i = 0; i < N * N; ++i) {
-    //         h_A[i] = i;
-    //         h_B[i] = i;
-    //     }
-    // }else{
-    //     printf("[ERR]:Errore nell'allocazione delle matrici sull'host\n");
-    //     return 1;
-    // }
+    // Inizializza le matrici A e B sull'host
+    if(h_A != NULL && h_B != NULL){
+        printf("[INFO]: Inizializzazione delle matrici sull'host\n");
+        for (int i = 0; i < N * N; ++i) {
+            h_A[i] = i;
+            h_B[i] = i;
+        }
+    }else{
+        printf("[ERR]:Errore nell'allocazione delle matrici sull'host\n");
+        return 1;
+    }
 
     // //Allocazione sul device (GPU)
-    // float *d_A = NULL, *d_B = NULL, *d_C = NULL;
-    // cudaError_t err1 = checkCudaError(cudaMalloc((void **)&d_A, matrix_size), "Allocazione matrice A su GPU");
-    // cudaError_t err2 = checkCudaError(cudaMalloc((void **)&d_B, matrix_size), "Allocazione matrice B su GPU");
-    // cudaError_t err3 = checkCudaError(cudaMalloc((void **)&d_C, matrix_size), "Allocazione matrice C su GPU");
+    float *d_A = NULL;
+    float *d_B = NULL;
+    float *d_C = NULL;
+    cudaError_t err1 = checkCudaError(cudaMalloc((void **)&d_A, matrix_size), "Allocazione matrice A su GPU");
+    cudaError_t err2 = checkCudaError(cudaMalloc((void **)&d_B, matrix_size), "Allocazione matrice B su GPU");
+    cudaError_t err3 = checkCudaError(cudaMalloc((void **)&d_C, matrix_size), "Allocazione matrice C su GPU");
 
-    // // Copia delle matrici dall'host alla GPU
-    // if(err1 == cudaSuccess && err2 == cudaSuccess && err3 == cudaSuccess){
-    //     printf("\n[INFO]Copia delle matrici sulla GPU\n");
-    //     checkCudaError(cudaMemcpy(d_A, h_A, matrix_size, cudaMemcpyHostToDevice), "Copia matrice A sulla GPU");
-    //     checkCudaError(cudaMemcpy(d_B, h_B, matrix_size, cudaMemcpyHostToDevice), "Copia matrice B sulla GPU");
-    //     printf("Matrici copiate sulla GPU\n");
-    // }else{
-    //     printf("[ERR]: Errore nell'allocazione delle matrici sulla GPU\n");
-    //     return 2;
-    // }
-    // //Indicatori di performance
-    // float cublasMillis = 0;
-    // double cublasTFLOPS = 0;
+    // Copia delle matrici dall'host alla GPU
+    if(err1 == cudaSuccess && err2 == cudaSuccess && err3 == cudaSuccess){
+        printf("\n[INFO]: Copia delle matrici sulla GPU\n");
+        checkCudaError(cudaMemcpy(d_A, h_A, matrix_size, cudaMemcpyHostToDevice), "Copia matrice A sulla GPU");
+        checkCudaError(cudaMemcpy(d_B, h_B, matrix_size, cudaMemcpyHostToDevice), "Copia matrice B sulla GPU");
+        printf("[INFO]: Matrici copiate sulla GPU\n");
+    }else{
+        printf("[ERR]: Errore nell'allocazione delle matrici sulla GPU\n");
+        return 2;
+    }
+    //Indicatori di performance
+    float cublasMillis = 0;
+    double cublasTFLOPS = 0;
 
-    // float myMillis = 0;
-    // double myTFLOPS = 0;
+    float myMillis = 0;
+    double myTFLOPS = 0;
 
-    // ///////////////////// ALGORHITMs ///////////////////////
-    // /////// cuBLAS ///////
-    // // Moltiplicazione di matrici con cuBLAS
-    // cublasMatMul(d_A, d_B, d_C, N, &cublasMillis, &cublasTFLOPS); 
-    // // Copia dei risultati dalla GPU all'host
-    // checkCudaError(cudaMemcpy(h_C_cublas, d_C, matrix_size, cudaMemcpyDeviceToHost), "Copia matrice C dall'host");
-    // //Stampa delle matrici
-    // if(N <= 4){
-    //     printf("Matrice A:\n");
-    //     printMat(h_A, N, N);
-    //     printf("Matrice B:\n");
-    //     printMat(h_B, N, N);
-    //     printf("Matrice C:\n");
-    //     printMat(h_C_cublas, N, N);
-    // }
+    ///////////////////// ALGORHITMs ///////////////////////
+    /////// cuBLAS ///////
+    // Moltiplicazione di matrici con cuBLAS
+    cublasMatMul(d_A, d_B, d_C, N, &cublasMillis, &cublasTFLOPS); 
+    // Copia dei risultati dalla GPU all'host
+    checkCudaError(cudaMemcpy(h_C_cublas, d_C, matrix_size, cudaMemcpyDeviceToHost), "Copia matrice C dall'host");
+    //Stampa delle matrici
+    if(N <= 4){
+        printf("Matrice A:\n");
+        printMat(h_A, N, N);
+        printf("Matrice B:\n");
+        printMat(h_B, N, N);
+        printf("Matrice C:\n");
+        printMat(h_C_cublas, N, N);
+    }
 
-    // // Stampa dei risultati
-    // printf("\n\nTempo di esecuzione [cuBLAS] [size: %d]: %f ms\n", cublasMillis, N);
-    // printf("TFLOPS [cuBLAS] [size: %d]: %f\n", cublasTFLOPS, N);
+    // Stampa dei risultati
+    printf("\n\nTempo di esecuzione [cuBLAS] [size: %d]: %f ms\n", cublasMillis, N);
+    printf("TFLOPS [cuBLAS] [size: %d]: %f\n", cublasTFLOPS, N);
     
-    // //Libero la memoria delle matrici sorgenti
-    // cudaFree(d_A);
-    // cudaFree(d_B);
+    //Libero la memoria delle matrici sorgenti
+    cudaFree(d_A);
+    cudaFree(d_B);
 
 
-    // /////// Custom Kernel ///////
-    // // Moltiplicazione di matrici con kernel custom
-    // tensorCoreMatMul(h_A, h_B, d_C, N, &myMillis, &myTFLOPS);
-    // // Copia dei risultati dalla GPU all'host
-    // checkCudaError(cudaMemcpy(h_C_wmma, d_C, matrix_size, cudaMemcpyDeviceToHost), "Copia matrice C dal device");
-    // //Stampa delle matrici
-    // if(N <= 4){
-    //     printf("Matrice A:\n");
-    //     printMat(h_A, N, N);
-    //     printf("Matrice B:\n");
-    //     printMat(h_B, N, N);
-    //     printf("Matrice C:\n");
-    //     printMat(h_C_wmma, N, N);
-    // }
+    /////// Custom Kernel ///////
+    // Moltiplicazione di matrici con kernel custom
+    tensorCoreMatMul(h_A, h_B, d_C, N, &myMillis, &myTFLOPS);
+    // Copia dei risultati dalla GPU all'host
+    checkCudaError(cudaMemcpy(h_C_wmma, d_C, matrix_size, cudaMemcpyDeviceToHost), "Copia matrice C dal device");
+    //Stampa delle matrici
+    if(N <= 4){
+        printf("Matrice A:\n");
+        printMat(h_A, N, N);
+        printf("Matrice B:\n");
+        printMat(h_B, N, N);
+        printf("Matrice C:\n");
+        printMat(h_C_wmma, N, N);
+    }
 
-    // // Stampa dei risultati
-    // printf("\n\nTempo di esecuzione [wmma] [size: %d]: %f ms\n", myMillis, N);
-    // printf("TFLOPS [wmma] [size: %d]: %f\n", myTFLOPS, N);
+    // Stampa dei risultati
+    printf("\n\nTempo di esecuzione [wmma] [size: %d]: %f ms\n", myMillis, N);
+    printf("TFLOPS [wmma] [size: %d]: %f\n", myTFLOPS, N);
 
-    // //Testing dei risultati e confronto con cuBLAS
-    // for(int i = 0; i < N * N; i++){
-    //     if(h_C_cublas[i] != h_C_wmma[i]){
-    //         printf("Errore: i risultati non coincidono\n");
-    //         break;
-    //     }
-    // }
+    //Testing dei risultati e confronto con cuBLAS
+    for(int i = 0; i < N * N; i++){
+        if(h_C_cublas[i] != h_C_wmma[i]){
+            printf("Errore: i risultati non coincidono\n");
+            break;
+        }
+    }
 
 
-    // // Libera la memoria sull'host
-    // free(h_A);
-    // free(h_B);
-    // free(h_C_cublas);
-    // free(h_C_wmma);
+    // Libera la memoria sull'host
+    free(h_A);
+    free(h_B);
+    free(h_C_cublas);
+    free(h_C_wmma);
 
-    // // Libera la memoria sulla GPU
-    // cudaFree(d_C);
+    // Libera la memoria sulla GPU
+    cudaFree(d_C);
 }
 
 #endif
