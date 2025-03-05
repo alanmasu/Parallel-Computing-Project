a6b876710f45b3c29e92e9f24d0a556b1328aef9
alanmasu <alan.masutti@gmail.com>
2025-03-05 19:03:42 +0100

    Modificato Makefile e creato primo codice di testing [NON VA]

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-# Nome dell'eseguibile
+# Nome dell'eseguibile principale
 TARGET = main
 
 # Directory
@@ -7,20 +7,23 @@ LIB_DIR = lib
 OBJ_DIR = build/obj
 BIN_DIR = build/bin
 OUTPUT_DIR = run
+TEST_DIR = test
+TEST_BIN_DIR = $(BIN_DIR)/test
 
 # Trova tutti i file sorgenti .cu in src e nelle sottodirectory di lib
 SRCS = $(wildcard $(SRC_DIR)/*.cu) $(wildcard $(LIB_DIR)/*/*.cu)
 
 # Crea i file oggetto corrispondenti mantenendo la struttura delle directory
-OBJS = $(patsubst %.cu,$(OBJ_DIR)/%.o,$(notdir $(SRCS)))
+OBJS = $(patsubst $(SRC_DIR)/%.cu,$(OBJ_DIR)/src/%.o,$(wildcard $(SRC_DIR)/*.cu)) \
+       $(patsubst $(LIB_DIR)/%/%.cu,$(OBJ_DIR)/lib/%/%.o,$(wildcard $(LIB_DIR)/*/*.cu))
 
 # Trova tutte le sottodirectory in lib e le aggiunge al percorso degli include
 INCLUDE_DIRS = $(shell find $(LIB_DIR) -type d)
-INCLUDE_FLAGS = $(addprefix -I, $(INCLUDE_DIRS))
+INCLUDE_FLAGS = $(addprefix -I, $(INCLUDE_DIRS)) -I$(LIB_DIR)
 
 # Compilatore e flags
 NVCC = nvcc
-NVCC_FLAGS = -O3 -lineinfo $(INCLUDE_FLAGS) -lcublas -arch=sm_80 -DWMMA_BATCHED -DTESTING -DTESTING_BATCHED
+NVCC_FLAGS = -O3 -lineinfo $(INCLUDE_FLAGS) -lcublas -arch=sm_80
 
 main: all
 
@@ -29,26 +32,46 @@ $(BIN_DIR):
 	mkdir -p $(BIN_DIR)
 
 $(OBJ_DIR):
-	mkdir -p $(OBJ_DIR)
+	mkdir -p $(OBJ_DIR)/src $(OBJ_DIR)/lib
 
 # Regola di default
 all: $(BIN_DIR) $(OBJ_DIR) $(BIN_DIR)/$(TARGET)
 
 # Compilazione dei file oggetto dalla directory src
-$(OBJ_DIR)/%.o: $(SRC_DIR)/%.cu
+$(OBJ_DIR)/src/%.o: $(SRC_DIR)/%.cu
 	$(NVCC) $(NVCC_FLAGS) -c $< -o $@
 
-# Compilazione dei file oggetto dalle sottodirectory di lib
-$(OBJ_DIR)/%.o: $(LIB_DIR)/%/*.cu
+# Compilazione dei file oggetto dalle sottodirectory di lib (come lib/matMul)
+$(OBJ_DIR)/lib/%/%.o: $(LIB_DIR)/%/%.cu
+	mkdir -p $(OBJ_DIR)/lib/$*
 	$(NVCC) $(NVCC_FLAGS) -c $< -o $@
 
-# Link e generazione dell'eseguibile
+# Link e generazione dell'eseguibile principale
 $(BIN_DIR)/$(TARGET): $(OBJS)
 	$(NVCC) $(NVCC_FLAGS) $(OBJS) -o $@
 
+# --------------------------------
+# Sezione per i test
+# --------------------------------
+
+# Trova tutte le sottodirectory in test/
+TESTS = $(shell find $(TEST_DIR) -mindepth 1 -maxdepth 1 -type d)
+TEST_BINS = $(patsubst $(TEST_DIR)/%, $(TEST_BIN_DIR)/%, $(TESTS))
+
+# Regola per creare tutti i test
+test: $(TEST_BIN_DIR) $(TEST_BINS)
+
+# Regola per creare la directory dei binari dei test
+$(TEST_BIN_DIR):
+	mkdir -p $(TEST_BIN_DIR)
+
+# Regola per compilare ogni test con le librerie di lib/
+$(TEST_BIN_DIR)/%: $(TEST_DIR)/% $(OBJS)
+	$(NVCC) $(NVCC_FLAGS) $(wildcard $</*.cu) $(OBJS) -o $@
+
 # Pulizia dei file generati
 clean:
-	rm -rf $(OUTPUT_DIR) 
+	rm -rf $(OUTPUT_DIR)
 	mkdir -p $(OUTPUT_DIR)
 
 # Pulizia dei file oggetto e dell'eseguibile
---
+++ test/test_batched/test_batched.cu
@@ -0,0 +1,113 @@
+//Simple Matrix Multiplication whit cuBLAS
+
+#include <stdio.h>
+#include <cuda_runtime.h>
+#include <cublas_v2.h>
+#include <matMul.h>
+#include <ctime>
+#include <Utilities.h>
+
+#warning "TEST BATCHED"
+
+#define N 64
+
+int main(int argc, char **argv){
+    // Puntatori per le matrici sull'host
+    float *h_A = NULL;
+    float *h_B = NULL;
+    float *h_C_cublas = NULL;
+    float *h_C_wmma = NULL;
+
+    // // Allocazione delle matrici sull'host (CPU)
+    printf("\n--------- TESTING CODE ---------\n");
+    printf("[INFO]: Allocazione delle matrici sull'host\n");
+    size_t matrix_size = N * N * sizeof(float);
+    h_A = (float *)malloc(matrix_size);
+    h_B = (float *)malloc(matrix_size);
+    h_C_cublas = (float *)malloc(matrix_size);
+    h_C_wmma = (float *)malloc(matrix_size);
+    printf("[INFO]: Allocazione delle matrici sull'host completata\n");
+
+    // Inizializza le matrici A e B sull'host
+    if(h_A != NULL && h_B != NULL && h_C_wmma != NULL){
+        printf("[INFO]: Inizializzazione delle matrici sull'host\n");
+        for (int i = 0; i < N; ++i) {
+            for(int j = 0; j < N; ++j){
+                // h_A[i] = 0.1;
+                // h_B[i] = 0.2;                // Blocco [r,c]
+                if(i < 32  && j < 32){          // Blocco [0,0] [ERROR]
+                    h_A[i + j * N] = i + j * N;
+                    h_B[i + j * N] = i + j * N;
+                }else if(i >= 32 && j < 32){    // Blocco [0,1] [OK]
+                    h_A[i + j * N] = 0;
+                    h_B[i + j * N] = 0;
+                }else if(i < 32 && j >= 32){    // Blocco [1,0] [ERROR]
+                    h_A[i + j * N] = 0;
+                    h_B[i + j * N] = 0;
+                }else{                          // Blocco [1,1] [OK]
+                    h_A[i + j * N] = 0;
+                    h_B[i + j * N] = 0;
+                }
+            }
+        }
+        printf("A[0,1]: %f\nA[0,32]: %f\nA[32,0]: %f\nA[32,32]: %f\n\n", h_A[1], h_A[32], h_A[32 * N], h_A[32 * N + 32]);
+        memset(h_C_wmma, 0, matrix_size);
+    }else{
+        printf("[ERR]:Errore nell'allocazione delle matrici sull'host\n");
+        return 1;
+    }
+
+    // //Allocazione sul device (GPU)
+    float *d_A = NULL;
+    float *d_B = NULL;
+    float *d_C = NULL;
+    cudaError_t err1 = checkCudaError(cudaMalloc((void **)&d_A, matrix_size), "Allocazione matrice A su GPU");
+    cudaError_t err2 = checkCudaError(cudaMalloc((void **)&d_B, matrix_size), "Allocazione matrice B su GPU");
+    cudaError_t err3 = checkCudaError(cudaMalloc((void **)&d_C, matrix_size), "Allocazione matrice C su GPU");
+
+    // Copia delle matrici dall'host alla GPU
+    if(err1 == cudaSuccess && err2 == cudaSuccess && err3 == cudaSuccess){
+        printf("\n[INFO]: Copia delle matrici sulla GPU\n");
+        checkCudaError(cudaMemcpy(d_A, h_A, matrix_size, cudaMemcpyHostToDevice), "Copia matrice A sulla GPU");
+        checkCudaError(cudaMemcpy(d_B, h_B, matrix_size, cudaMemcpyHostToDevice), "Copia matrice B sulla GPU");
+        checkCudaError(cudaMemcpy(d_C, h_C_wmma, matrix_size, cudaMemcpyHostToDevice), "Copia matrice C sulla GPU");
+        printf("[INFO]: Matrici copiate sulla GPU\n");
+    }else{
+        printf("[ERR]: Errore nell'allocazione delle matrici sulla GPU\n");
+        return 2;
+        }
+
+
+    //Indicatori di performance
+    float cublasMillis = 0;
+    double cublasTFLOPS = 0;
+
+    ///////////////////// ALGORHITMs ///////////////////////
+    /////// cuBLAS ///////
+    // Moltiplicazione di matrici con cuBLAS
+    cublasMatMul(d_A, d_B, d_C, N,&cublasMillis, &cublasTFLOPS); 
+    // Copia dei risultati dalla GPU all'host
+    checkCudaError(cudaMemcpy(h_C_cublas, d_C, matrix_size, cudaMemcpyDeviceToHost), "Copia matrice C dall'host");
+    //Stampa delle matrici
+    if(N <= 4){
+        printf("Matrice A:\n");
+        printMat(h_A, N, N);
+        printf("Matrice B:\n");
+        printMat(h_B, N, N);
+        printf("Matrice C:\n");
+        printMat(h_C_cublas, N, N);
+    }
+
+    // Stampa dei risultati
+    printf("\nTempo di esecuzione [cuBLAS] [size: %d]: %f ms\n", cublasMillis, N);
+    printf("TFLOPS [cuBLAS] [size: %d]: %f\n\n", cublasTFLOPS, N);
+    
+    //Libero la memoria delle matrici sorgenti
+    if(err1 == cudaSuccess && err2 == cudaSuccess){
+        cudaFree(d_A);
+        cudaFree(d_B);
+        d_A = NULL;
+        d_B = NULL;
+    }
+
+}
