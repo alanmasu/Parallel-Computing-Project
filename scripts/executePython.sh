ab8678c7c2e04ac07d9bfb15525748a579fd5f54
alanmasu <alan.masutti@gmail.com>
2025-04-06 16:04:04 +0200

    Creati gli script per i grafici

---
+++ scripts/executePython.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This script is used to execute a Python script in a specific environment
+# Usage: source executePython.sh <script_name> <args>
+PYTHON_ENV="${PWD}/graphEnv/bin/activate"
+
+# Activate the Python environment
+source $PYTHON_ENV
+
+# Check if the script name is provided
+if [ -z "$1" ]; then
+    echo "Usage: $0 <script_name> [args]"
+    exit 1
+fi
+# Check if the script exists    
+if [ ! -f "$1" ]; then
+    echo "Error: Script $1 not found!"
+    exit 1
+fi
+
+# Execute the Python script with the provided arguments
+python3 $1 "${@:2}"
+
+# Find __pycache__ directories and remove them
+find . -type d -name "__pycache__" -exec rm -rf {} +
+
+# Deactivate the Python environment
+deactivate
--- utils/createCharts.py
+++ utils/createCharts.py
@@ -1,13 +1,11 @@
-#importo le librerie
-# import matplotlib.pyplot as plt
-# import numpy as np
-# import pandas as pd
-# import seaborn as sns
+# Per ogni file .csv presente nella cartella /results crea una cartella con lo stesso nome del file .csv
+# e al suo interno salva i grafici
 
-#per ogni file .csv presente nella cartella /results crea una cartella con lo stesso nome del file .csv
-#e al suo interno salva i grafici
+# Importo le librerie
 import os
 import shutil
+import pythonCharts as charts
+import argparse
 
 # Funzione principale
 def crea_cartelle_e_sposta_file(directory):
@@ -45,6 +43,37 @@ def crea_cartelle_e_sposta_file(directory):
             else:
                 print(f"Il file {file_txt} non esiste per il file {file}")
 
-# Esempio di utilizzo
-directory_input = "./results"
-crea_cartelle_e_sposta_file(directory_input)
+# Cicla sulle cartelle create e crea i grafici
+def creaGrafici(directory, force):
+    for folder in os.listdir(directory):
+        if os.path.isdir(os.path.join(directory, folder)):
+            # Controlla se esistono delle immagini nella cartella
+            images = [f for f in os.listdir(os.path.join(directory, folder)) if f.endswith('.png')]
+            if images and force == False:
+                print(f"Le immagini sono già state create per {folder}.")
+                continue
+            
+            # Controlla se il file .csv esiste nella cartella
+            file_csv = os.path.join(directory, folder, f"result-{folder[7:]}.csv")
+            if os.path.exists(file_csv):
+                # Crea il grafico
+                res = charts.createFlopsChart(file_csv, os.path.join(directory, folder, "chart"))
+                if res:
+                    print(f"Creato grafico per {file_csv}")
+            else:
+                print(f"Il file {file_csv} non esiste.")
+    
+
+## Main
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser(description='Create charts from results')
+    argparser.add_argument('-f', '--force', action='store_true', help='Force creation of charts even if they already exist')
+    argparser.add_argument('-d', '--directory', type=str, help='Directory to create charts from', default='./results')
+    args = argparser.parse_args()
+    
+    force = args.force
+    directory_input = args.directory
+    
+    charts.seabornConfig()
+    crea_cartelle_e_sposta_file(directory_input)
+    creaGrafici(directory_input, force)
